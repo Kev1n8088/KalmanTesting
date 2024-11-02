de439b0ce1bd18e23ea4d6cff36eecca9a1096db
Kev1n8088 <jebidiahkerman828@gmail.com>
2024-11-02 17:36:21 -0400

    Completed orientation by integration of gyro. Added accel quanternion update function for grav vector measurement before launch. Changed var names for consistency

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,20 @@
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include "Adafruit_BMP3XX.h"
-#include "SparkFun_BNO080_Arduino_Library.h"
-#include <Kalman.h> // Include the Kalman library by Romain Fetick
-#include <math.h>   // Include math library for trigonometric functions
+#include "SparkFun_BNO08x_Arduino_Library.h"
+
+#include <Kalman.h>
+#include <math.h> // Include math library for trigonometric functions
+
+using namespace BLA;
+
+#define BNO08X_INT -1
+#define BNO08X_RST -1
+
+// #define BNO08X_ADDR 0x4B  // SparkFun BNO08x Breakout (Qwiic) defaults to 0x4B
+#define BNO08X_ADDR 0x4A // Alternate address if ADR jumper is closed
+
+BNO08x IMU;
 
 #define SEALEVELPRESSURE_HPA (1013.25)
 
@@ -12,17 +23,21 @@ float gyroBias[3] = {0, 0, 0};
 float oldGyroBias[3] = {0, 0, 0};
 
 float gyroMeasurement[3] = {0, 0, 0};
-float eulerOrientation[3] = {0, 0, 0};
-float quanterionOrientation[4] = {0, 0, 0, 0};
+float filteredGyro[3] = {0, 0, 0};
+float orientationEuler[3] = {0, 0, 0};
+float orientationQuanternion[4] = {0, 0, 0, 0};
 
 float baroMeaurement = 0; // baro measurement in meters adjusted by sealevel HPA
 
+float accelMeasurement[3] = {0, 0, 0};
+float oldAccelQuaternion[4] = {0, 0, 0, 0};
+float accelQuaternion[4] = {0, 0, 0, 0};
+
 // put function declarations here:
 
-BNO080 IMU;
+#define BNO08X_RESET -1
 
 Adafruit_BMP3XX bmp;
-
 void updateBiases()
 {
   // called every 5 seconds, updates gyroBias with new gyroBias, and oldGyroBias with gyroBias. Always use oldGyroBias for launch.
@@ -30,39 +45,45 @@ void updateBiases()
   {
     oldGyroBias[i] = gyroBias[i];
   }
-
-  gyroBias[0] = IMU.getUncalibratedGyroBiasX();
-  gyroBias[1] = IMU.getUncalibratedGyroBiasY();
-  gyroBias[2] = IMU.getUncalibratedGyroBiasZ();
+  gyroBias[0] = IMU.getUncalibratedGyroX();
+  gyroBias[1] = IMU.getUncalibratedGyroY();
+  gyroBias[2] = IMU.getUncalibratedGyroZ();
 }
 
 void getGyroRates()
 {
-  gyroMeasurement[0] = IMU.getGyroX();
-  gyroMeasurement[1] = IMU.getGyroY();
-  gyroMeasurement[2] = IMU.getGyroZ();
+  gyroMeasurement[0] = IMU.getUncalibratedGyroX();
+  gyroMeasurement[1] = IMU.getUncalibratedGyroY();
+  gyroMeasurement[2] = IMU.getUncalibratedGyroZ();
 }
 
 void gryoFilter()
 {
-  /*eulerOrientation[0] = kalmanX.getAngle(gyroMeasurement[0] - oldGyroBias[0], 0.01);
-  eulerOrientation[1] = kalmanY.getAngle(gyroMeasurement[1] - oldGyroBias[1], 0.01);
-  eulerOrientation[2] = kalmanZ.getAngle(gyroMeasurement[2] - oldGyroBias[2], 0.01);*/
+  filteredGyro[0] = gyroMeasurement[0];
+  filteredGyro[1] = gyroMeasurement[1];
+  filteredGyro[2] = gyroMeasurement[2];
+}
+
+void gyroEuler(float dt)
+{
+  orientationEuler[0] += (filteredGyro[0] * dt);
+  orientationEuler[1] += (filteredGyro[1] * dt);
+  orientationEuler[2] += (filteredGyro[2] * dt);
 }
 
 void gyroQuanternion()
 {
-  float cy = cos(eulerOrientation[2] * 0.5);
-  float sy = sin(eulerOrientation[2] * 0.5);
-  float cp = cos(eulerOrientation[1] * 0.5);
-  float sp = sin(eulerOrientation[1] * 0.5);
-  float cr = cos(eulerOrientation[0] * 0.5);
-  float sr = sin(eulerOrientation[0] * 0.5);
+  float cy = cos(orientationEuler[2] * 0.5);
+  float sy = sin(orientationEuler[2] * 0.5);
+  float cp = cos(orientationEuler[1] * 0.5);
+  float sp = sin(orientationEuler[1] * 0.5);
+  float cr = cos(orientationEuler[0] * 0.5);
+  float sr = sin(orientationEuler[0] * 0.5);
 
-  quanterionOrientation[0] = cr * cp * cy + sr * sp * sy;
-  quanterionOrientation[1] = sr * cp * cy - cr * sp * sy;
-  quanterionOrientation[2] = cr * sp * cy + sr * cp * sy;
-  quanterionOrientation[3] = cr * cp * sy - sr * sp * cy;
+  orientationQuanternion[0] = cr * cp * cy + sr * sp * sy;
+  orientationQuanternion[1] = sr * cp * cy - cr * sp * sy;
+  orientationQuanternion[2] = cr * sp * cy + sr * cp * sy;
+  orientationQuanternion[3] = cr * cp * sy - sr * sp * cy;
 }
 
 void getBaro()
@@ -70,21 +91,57 @@ void getBaro()
   baroMeaurement = bmp.readAltitude(SEALEVELPRESSURE_HPA);
 }
 
+void updateAccelQuaternion()
+{
+  // called every 5 seconds, updates accelQuanterion with new accelQuanterion, and oldaccelQuanterion with accelQuanterion. Always use oldaccelQuanterion for launch.
+
+  for (int i = 0; i < 3; i++)
+  {
+    oldAccelQuaternion[i] = accelQuaternion[i];
+  }
+
+  accelMeasurement[0] = IMU.getAccelX();
+  accelMeasurement[1] = IMU.getAccelY();
+  accelMeasurement[2] = IMU.getAccelZ();
+
+  float cy = cos(accelMeasurement[2] * 0.5);
+  float sy = sin(accelMeasurement[2] * 0.5);
+  float cp = cos(accelMeasurement[1] * 0.5);
+  float sp = sin(accelMeasurement[1] * 0.5);
+  float cr = cos(accelMeasurement[0] * 0.5);
+  float sr = sin(accelMeasurement[0] * 0.5);
+
+  accelQuaternion[0] = cr * cp * cy + sr * sp * sy;
+  accelQuaternion[1] = sr * cp * cy - cr * sp * sy;
+  accelQuaternion[2] = cr * sp * cy + sr * cp * sy;
+  accelQuaternion[3] = cr * cp * sy - sr * sp * cy;
+}
+
+unsigned long oldtime;
+unsigned long newtime;
+float dt;
+float printTime;
+
 void setup()
 {
   // put your setup code here, to run once:
   Serial.begin(115200);
+  while (!Serial)
+    delay(10); // will pause Zero, Leonardo, etc until serial console opens
+
   Wire.begin();
+  if (IMU.begin(BNO08X_ADDR, Wire, BNO08X_INT, BNO08X_RST) == false)
+  {
+    Serial.println("BNO08x not detected at default I2C address. Check your jumpers and the hookup guide. Freezing...");
+    while (1)
+      ;
+  }
 
-  Wire.setClock(400000); // Increase I2C data rate to 400kHz
-  IMU.begin();
-  IMU.enableUncalibratedGyro(50);
-
-  if (!bmp.begin_I2C())
+  /*if (!bmp.begin_I2C())
   { // hardware I2C mode, can pass in address & alt Wire
     // if (! bmp.begin_SPI(BMP_CS)) {  // hardware SPI mode
     // if (! bmp.begin_SPI(BMP_CS, BMP_SCK, BMP_MISO, BMP_MOSI)) {  // software SPI mode
-    Serial.println("Could not find a valid BMP3 sensor, check wiring!");
+    Serial.println("Could not find a valid BMP3 sensor, check wiring!");ff
     while (1)
       ;
   }
@@ -92,40 +149,90 @@ void setup()
   bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
   bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
   bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
-  bmp.setOutputDataRate(BMP3_ODR_50_HZ);
+  bmp.setOutputDataRate(BMP3_ODR_50_HZ);*/
+
+  oldtime = micros();
+  if (IMU.enableUncalibratedGyro(1000) == true)
+  {
+    Serial.println(F("Gyro enabled"));
+  }
+  else
+  {
+    Serial.println("Could not enable gyro");
+  }
+}
+
+void setReports()
+{
+
+  if (IMU.enableUncalibratedGyro(1000) == true)
+  {
+    Serial.println(F("Gyro enabled"));
+  }
+  else
+  {
+    Serial.println("Could not enable gyro");
+  }
+  if (IMU.enableAccelerometer(1000) == true)
+  {
+    Serial.println(F("Accelerometer enabled"));
+  }
+  else
+  {
+    Serial.println("Could not enable accelerometer");
+  }
+}
+
+void printGyroData()
+{
+  Serial.print("Orientation X: ");
+  Serial.print(orientationEuler[0]);
+  Serial.print(" Y: ");
+  Serial.print(orientationEuler[1]);
+  Serial.print(" Z: ");
+  Serial.println(orientationEuler[2]);
+  printTime = millis();
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
-  if (!IMU.dataAvailable())
+  if (IMU.getSensorEvent() == true)
   {
-    return;
+    Serial.println("Sensor event");
+    getGyroRates();
+    gryoFilter();
+    newtime = micros();
+    dt = newtime - oldtime;
+    gyroEuler(dt / 1000000);
+    oldtime = newtime;
+
+    // gyroQuanternion();
+    //  Now orientationEuler contains the filtered orientation data
+    updateAccelQuaternion();
   }
-  getGyroRates();
-  gryoFilter();
-  gyroQuanternion();
-  // Now eulerOrientation contains the filtered orientation data
-  Serial.print("Orientation X: ");
-  Serial.print(eulerOrientation[0]);
-  Serial.print(" Y: ");
-  Serial.print(eulerOrientation[1]);
-  Serial.print(" Z: ");
-  Serial.print(eulerOrientation[2]);
-  // Now quanterionOrientation contains the updated quaternion orientation data
-  Serial.print("   Quaternion W: ");
-  Serial.print(quanterionOrientation[0]);
+
+  if (millis() - printTime > 50)
+  {
+    // Serial.println(filteredGyro[0]);
+    printGyroData();
+    // Serial.print(filteredGyro[0]);
+  }
+
+  // Now orientationQuanternion contains the updated quaternion orientation data
+  /*Serial.print("   Quaternion W: ");
+  Serial.print(orientationQuanternion[0]);
   Serial.print(" X: ");
-  Serial.print(quanterionOrientation[1]);
+  Serial.print(orientationQuanternion[1]);
   Serial.print(" Y: ");
-  Serial.print(quanterionOrientation[2]);
+  Serial.print(orientationQuanternion[2]);
   Serial.print(" Z: ");
-  Serial.println(quanterionOrientation[3]);
+  Serial.println(orientationQuanternion[3]);*
 
   if (!bmp.performReading())
   {
     return;
   }
 
-  getBaro();
+  getBaro();*/
 }
